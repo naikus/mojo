4b24b29254aae4af1f29613acc35ff1bcecee07c
naikus <aniketn3@gmail.com>
2012-02-24 16:37:23 +0530

    Initial commit

---
+++ src/datalist.js
@@ -0,0 +1,205 @@
+/**
+ * The DataList widget. Creates a selectable list from the specified data
+ * @Usage 
+ * $.("selector").DataList(options);
+ * The options object takes the following:
+ * values:
+ * {
+ *    listClass: css class for list, default "list",
+ *    itemClass: css class for list item, default "list-item",
+ *    data: The data array, default empty array
+ *    render: A function to render list item "function(list-widget, $(curr-item), index, data[i])"
+ *    onselectionchange: a handler function called when list selection changes
+ * }
+ * @author aniket
+ */
+(function($) {
+   var defaults = {
+      listClass: "list",
+      itemClass: "list-item",
+      data: [],
+      render: function(list, item, idx, datum) {
+         return "" + datum + "";
+      },
+      onselectionchange: function() {}
+   },
+   isTypeOf = $.isTypeOf,
+   forEach = $.forEach,
+   uuid = $.uuid;
+   
+   /**
+    * Render each item using the specified renderer
+    */
+   function renderItem(widget, objItem, itemIdx, opts)  {
+      var li = document.createElement("li"), item = $(li), content;
+      item.data("model", objItem);
+      item.data("index", itemIdx);
+
+      item.addClass(opts.itemClass);
+      item.attr("id", "li" + uuid());
+      content = opts.render(widget, item, itemIdx, objItem);
+
+      // check if the renderer has already appended
+      if(!item.html()) {
+         if(isTypeOf(content, "String"))   {
+            item.html(content);
+         }else {
+            item.append(content);
+         }
+      }
+      return item;
+   }
+   
+   $.extension("DataList", function(options) {
+      // these are our final options
+      var opts = $.extend({}, defaults, options),
+      // copy the data array
+      data = opts.data.slice(0), 
+      selectedItem, allItems,
+      // our root element, create it if not present
+      listRoot,
+      // support touch/tap events?
+      action = document.createTouch ? "tap" : "click",
+      enabled = true,
+      element = this.get(0), 
+      ul, widget;
+      
+      /**
+       * Render the entire list widget
+       */
+      function render() {
+         allItems = [];
+         // @TODO remove all event listeners before removing items
+         listRoot.html("");
+         
+         if(data && data.length)   {
+            var items = document.createDocumentFragment(); //not supported in IE 5.5
+            forEach(data, function(datum, i) {
+               var $li = renderItem(widget, datum, i, opts);
+               items.appendChild($li.get(0));
+               allItems[allItems.length] = $li;
+            });
+            listRoot.append(items);
+         }
+      }
+      
+      function fireSelectionChanged(item)  {
+         var old = null, ret;
+         if(selectedItem)  {
+            old = selectedItem;
+            if(selectedItem === item)  {
+               return;
+            }
+         }
+         // this is needed so that onselection change handlers can can call this.getSelectedItem
+         selectedItem = item; 
+         
+         ret = opts.onselectionchange.call(widget, item, old);
+         if(ret !== false) {
+            if(old) {
+               old.removeClass("selected");
+            }
+            selectedItem = item;
+            selectedItem.addClass("selected");
+         }else {
+            selectedItem = old;
+         }
+      }
+      
+      if(element.tagName.toLowerCase() === "ul")  {
+         listRoot = this;
+         ul =listRoot.get(0);
+      }else {
+         ul = document.createElement("ul");
+         this.append(ul);
+         listRoot = $(ul);
+      }
+      listRoot.addClass(opts.listClass);
+      
+      listRoot.on(action, function(e) {
+         var t = e.target, parent = t.parentNode, idx, item;
+         if(parent === ul) {
+            item = t;
+         }else {
+            while(parent && parent !== ul) {
+               item = parent;
+               parent = parent.parentNode;
+            }
+         }
+         idx = $(item).data("index");
+         if(typeof idx !== "undefined") {
+            item = allItems[idx];
+            fireSelectionChanged(item);
+         }
+      });
+      
+      // our public API that is exposed to the widget
+      widget = {
+         getElement: function() {
+            return listRoot;
+         },
+         
+         setItems: function(itemData) {
+            listRoot.html("");
+            data = itemData || [];
+            selectedItem = null;
+            render();
+         },
+         
+         setEnabled: function(bEnabled)  {
+            enabled = bEnabled === false ? false : true;
+            if(enabled) {
+               listRoot.removeClass("disabled");
+            }else {
+               listRoot.addClass("disabled");
+            }
+         },
+         
+         getItems: function() {
+            return data.slice(0);
+         },
+
+         getSelectedItem: function() {
+            return selectedItem;
+         },
+         
+         getSelectedIndex: function() {
+            var i, len;
+            if(!selectedItem) {
+               return -1;
+            }
+            for(i = 0, len = allItems.length; i < len && allItems[i] !== selectedItem; i++);
+            return i === len ? -1 : i;
+         },
+
+         selectItemAt: function(idx)   {
+            var len = allItems.length;
+            if(idx < len && idx >= 0)  {
+               fireSelectionChanged(allItems[idx]);
+            }
+         },
+         
+         setItemAt: function(idx, datum) {
+            var itm = allItems[idx], content;
+            if(itm) {
+               data[idx] = datum;
+               itm.data("model", datum);
+
+               content = opts.render(widget, itm, idx, datum);
+               // check if the renderer has already appended
+               if(content) {
+                  if(isTypeOf(content, "String"))   {
+                     itm.html(content);
+                  }else {
+                     itm.append(content);
+                  }
+               }
+            }
+         }
+      };
+      
+      render();
+      
+      return widget;
+   });
+})(h5);
---
+++ src/template.js
@@ -0,0 +1,126 @@
+/**
+ * A simple templating mechanism for creating string templates. A template has replacement tokens
+ * that are of the format '{' followed by anystring followed by '}'. 
+ * @example
+ * var temp = $.template("Hi, my name is {firstName} {lastName}, born in {birthYear}. " + 
+ *    "I live on the {address.street} street. Today I'm {getAge} years old.");
+ * var out = temp.process({
+ *    firstName: "John",
+ *    lastName:  "Doe",
+ *    birthYear: 1975,
+ *    getAge: function() {
+ *       return new Date().getYear() - this.birthYear;
+ *    },
+ *    address {
+ *       street: "1st"
+ *    } 
+ * });
+ * 
+ * // another way to create the template is:
+ * <script id="nameTemplate" type="text/x-mojo-template">
+ *    <p class="name">{firstName} {lastName}</p>
+ *    <p class="address">{address.street}</p>
+ * </script>
+ * // in js file:
+ * var templ = $("#nameTemplate").template();
+ * var out = templ.process({
+ *    firstName: "John",
+ *    lastName:  "Doe",
+ *    address {
+ *       street: "1st"
+ *    } 
+ * });
+ * 
+ * @author aniket
+ */
+(function($) {
+   var regExp = /\{([^\{\}]+)\}/g;
+   
+   /** 
+    * Internally compiles the template into a function so that we don't need to
+    * search the string for replacement for every call to process() function
+    */
+   function compile(templateStr) {
+      var match = regExp.exec(templateStr), allParts = [], tmp = templateStr, lastIndex = 0;
+      while(match !== null)   {
+         tmp = templateStr.substring(lastIndex, match.index);
+         if(tmp) {
+            allParts.push(tmp);
+         }
+         allParts.push({
+            rawKey:match[0], 
+            key: match[1]
+            });
+         lastIndex = regExp.lastIndex;
+         match = regExp.exec(templateStr);
+      }
+      // if there is any trailing string
+      if(lastIndex < templateStr.length) {
+         allParts.push(templateStr.substring(lastIndex));
+      }
+
+      return function(objMap) {
+         var str = [], i, len, part;
+         if(!objMap) {
+            return allParts.join("");
+         }
+         
+         for(i = 0, len = allParts.length; i < len; i++)  {
+            part = allParts[i];            
+            if(typeof(part) === "string")   {
+               str[str.length] = part;
+            }else {
+               str[str.length] = getValue(part.key, objMap);
+            }
+         }
+         return str.join("");
+      };
+   }
+   
+   function getValue(key, obj) {
+      var i, len, val, keys = key.split("."), tmp = obj, par;
+      for(i = 0, len = keys.length; i < len; i++) {
+         par = tmp;
+         val = tmp[keys[i]];
+         if(val == null) { // intentional '==' to check for null and undefined
+            break;
+         }else {
+            tmp = val;
+         }
+      }
+      if(!val && i < len) { // this means some object in the chain is null and we still have keys left
+         return "";
+      }
+      val = val || tmp;
+      if(typeof val === "function") {
+         return val.call(par);
+      }else {
+         return val;
+      }
+   }
+   
+   $.template = function(text) {
+      /* The original template string */
+      var templateStr = text,
+      templateFunc = compile(text);
+      
+      return {
+         template: templateStr,
+         /**
+          * Process this template. The values in the optional passed map will override those that were
+          * put by the put(String, String) function of this template
+          * @param {Object} objMap The object containing tokens and their values as properties
+          */
+         process: function(objMap) {
+            return templateFunc(objMap);
+         }
+      };
+   };
+   
+   $.extension("template", function() {
+      return $.template(this.html());
+   });
+})(h5);
+
+
+
